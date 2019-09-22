b5eb400ee164b5de3228561e27a1e13ec7530e03
ConorTheDev <con0r.b@protonmail.com>
2019-09-22 08:36:04 +0100

    :bookmark: Change version to 1.0-BETA

--- src/main/java/me/conorthedev/mediamod/util/Metadata.java
+++ src/main/java/me/conorthedev/mediamod/util/Metadata.java
@@ -6,6 +6,6 @@ package me.conorthedev.mediamod.util;
 public class Metadata {
     public static final String NAME = "MediaMod";
     public static final String MODID = "mediamod";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.0-BETA";
     public static final int VERSION_INT = 1;
 }
