676b780ee1fe92ac184d1133923ecb930e314d48
ConorTheDev <con0r.b@protonmail.com>
2019-09-29 11:32:25 +0100

    :sparkles: Update webhook messages

--- src/main/java/me/conorthedev/mediamod/util/Metadata.java
+++ src/main/java/me/conorthedev/mediamod/util/Metadata.java
@@ -6,6 +6,6 @@ package me.conorthedev.mediamod.util;
 public class Metadata {
     public static final String NAME = "MediaMod";
     public static final String MODID = "mediamod";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1   .0";
     public static final int VERSION_INT = 1;
 }
