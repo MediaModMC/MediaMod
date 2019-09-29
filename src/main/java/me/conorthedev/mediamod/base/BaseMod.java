8c7bd8253ea07f85082d5c715f08e2295167db6e
AmpTheDev <gerritegberink3@gmail.com>
2019-09-29 18:21:35 +0200

    analytics

--- src/main/java/me/conorthedev/mediamod/base/BaseMod.java
+++ src/main/java/me/conorthedev/mediamod/base/BaseMod.java
@@ -1,14 +1,23 @@
 package me.conorthedev.mediamod.base;
 
-import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import me.conorthedev.mediamod.util.Metadata;
+import net.minecraft.util.CryptManager;
 import net.minecraftforge.fml.client.FMLClientHandler;
 
+import javax.crypto.SecretKey;
+import javax.naming.AuthenticationException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.PublicKey;
+import java.util.Base64;
 import java.util.stream.Collectors;
 
 /**
@@ -17,23 +26,77 @@ import java.util.stream.Collectors;
  * @author ConorTheDev
  */
 public class BaseMod {
-    public static final String ENDPOINT = "https://api.conorthedev.me";
+    public static final String ENDPOINT = "http://localhost:3000";
 
     /**
      * Registers with api.conorthedev.me
      */
     public static boolean init() {
+
         try {
+            // Execute Mojang authentication, similar to when you join a server
+            URL authDataUrl = new URL(ENDPOINT + "/mediamod/analytics/authdata"); // get authentication data to send to Mojang's authentication servers
+
+            HttpURLConnection authDataCon = (HttpURLConnection) authDataUrl.openConnection();
+            authDataCon.setRequestMethod("GET");
+            authDataCon.setRequestProperty("User-Agent", "MediaMod/1.0");
+            authDataCon.connect();
+
+            BufferedReader authDataReader = new BufferedReader(new InputStreamReader(authDataCon.getInputStream()));
+            String authDataContent = authDataReader.lines().collect(Collectors.joining());
+
+            authDataReader.close();
+            authDataCon.disconnect();
+
+            JsonObject object = new JsonParser().parse(authDataContent).getAsJsonObject();
+
+            SecretKey secretKey = CryptManager.createNewSharedKey();
+            String serverId = object.get("server_id").getAsString();
+            PublicKey publicKey = CryptManager.decodePublicKey(Base64.getDecoder().decode(object.get("public_key").getAsString()));
+            String serverIdHash = new BigInteger(CryptManager.getServerIdHash(serverId, publicKey, secretKey)).toString(16);
+
+            HttpURLConnection authCon = (HttpURLConnection) new URL("https://sessionserver.mojang.com/session/minecraft/join").openConnection();
+            authCon.setRequestMethod("POST");
+            authCon.setRequestProperty("User-Agent", "MediaMod/1.0");
+            authCon.setRequestProperty("Content-Type", "application/json");
+            authCon.setDoOutput(true);
+            JsonObject data = new JsonObject();
+            data.addProperty("accessToken", FMLClientHandler.instance().getClient().getSession().getToken());
+            data.addProperty("selectedProfile", FMLClientHandler.instance().getClient().getSession().getPlayerID());
+            data.addProperty("serverId", serverIdHash);
+            authCon.connect();
+
+            try (OutputStream os = authCon.getOutputStream()) {
+                os.write(data.toString().getBytes(StandardCharsets.UTF_8));
+            }
+
+            BufferedReader authReader = new BufferedReader(new InputStreamReader(authCon.getInputStream()));
+            String authContent = authReader.lines().collect(Collectors.joining());
+            authReader.close();
+            authCon.disconnect();
+            if (authCon.getResponseCode() != 204) {
+                throw new AuthenticationException("Failed to authenticate with Mojang: " + authContent);
+            }
+
             // Create a conncetion
-            URL url = new URL(ENDPOINT + "/api/register/" + FMLClientHandler.instance().getClient().getSession().getProfile().getId().toString() + "/" + Metadata.MODID + "/" + Metadata.VERSION);
+            URL url = new URL(ENDPOINT + "/mediamod/analytics/register/" + FMLClientHandler.instance().getClient().getSession().getUsername() + "/" + Metadata.MODID + "/" + Metadata.VERSION);
 
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             // Set the request method
             con.setRequestMethod("POST");
             // Set the user agent
+            con.setRequestProperty("content-type", "application/json");
             con.setRequestProperty("user-agent", "MediaMod/1.0");
             // Connect to the API
+            con.setDoOutput(true);
             con.connect();
+
+            JsonObject newData = new JsonObject();
+            newData.addProperty("shared_secret", Base64.getEncoder().encodeToString(CryptManager.encryptData(publicKey, secretKey.getEncoded())));
+
+            try (OutputStream os = con.getOutputStream()) {
+                os.write(newData.toString().getBytes(StandardCharsets.UTF_8));
+            }
 
             // Read the output
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
@@ -44,33 +107,14 @@ public class BaseMod {
             con.disconnect();
 
             // Parse JSON
-            Gson g = new Gson();
-            RegisterResponse registerResponse = g.fromJson(content, RegisterResponse.class);
-
-            return registerResponse.uuid != null;
-        } catch (IOException ex) {
+            JsonObject response = new JsonParser().parse(content).getAsJsonObject();
+            if (!(response.has("success") && response.getAsJsonPrimitive("success").getAsBoolean())) {
+                throw new AuthenticationException("Failed to register with MediaMod: " + response.getAsJsonPrimitive("error").getAsString());
+            }
+            return true;
+        } catch (IOException | AuthenticationException ex) {
             ex.printStackTrace();
             return false;
-        }
-    }
-
-    private static class RegisterResponse {
-        final String uuid;
-        final ModResponse[] mods;
-
-        RegisterResponse(String uuid, ModResponse[] mods) {
-            this.uuid = uuid;
-            this.mods = mods;
-        }
-    }
-
-    private static class ModResponse {
-        final String id;
-        final String version;
-
-        ModResponse(String id, String version) {
-            this.id = id;
-            this.version = version;
         }
     }
 }
