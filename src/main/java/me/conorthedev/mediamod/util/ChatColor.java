94f41983b41b3f2557fc1e78ed17e14e1bf741b7
asbyth <cyronize@gmail.com>
2019-10-06 12:34:47 -0400

    wrong project

--- src/main/java/me/conorthedev/mediamod/util/ChatColor.java
+++ src/main/java/me/conorthedev/mediamod/util/ChatColor.java
@@ -79,17 +79,19 @@ public enum ChatColor {
      * Translate '§' into another character, such as '&', '*', etc, for more readability
      *
      * @param altColorChar    the new color code character
-     * @param textToTranslate the string to translate, could be '&cHyperium'
+     * @param textToTranslate the string to translate, could be '&cMediaMod'
      * @return translated string
      */
     public static String translateAlternateColorCodes(char altColorChar, String textToTranslate) {
         char[] b = textToTranslate.toCharArray();
+
         for (int i = 0; i < b.length - 1; i++) {
             if (b[i] == altColorChar && "0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(b[i + 1]) > -1) {
                 b[i] = ChatColor.COLOR_CHAR;
                 b[i + 1] = Character.toLowerCase(b[i + 1]);
             }
         }
+
         return new String(b);
     }
 
