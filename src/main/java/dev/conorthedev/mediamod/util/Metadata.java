f4281dcd79246ebdd9f759a05d61b29cb786b266
Conor Byrne <con0r.b@protonmail.com>
2020-05-19 21:14:26 +0100

    Change versionInt to 5

--- src/main/java/dev/conorthedev/mediamod/util/Metadata.java
+++ src/main/java/dev/conorthedev/mediamod/util/Metadata.java
@@ -7,5 +7,5 @@ public class Metadata {
     public static final String NAME = "MediaMod";
     public static final String MODID = "mediamod";
     public static final String VERSION = "1.2.1";
-    public static final int VERSION_INT = 3;
+    public static final int VERSION_INT = 5;
 }
