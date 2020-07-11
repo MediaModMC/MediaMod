0e847c413edd3f754bc6127c31c756a546f80b08
Conor Byrne <31479541+cbyrne@users.noreply.github.com>
2020-07-11 19:18:52 +0100

    Bump version to 1.2.4

--- src/main/java/dev/conorthedev/mediamod/util/Metadata.java
+++ src/main/java/dev/conorthedev/mediamod/util/Metadata.java
@@ -6,6 +6,6 @@ package dev.conorthedev.mediamod.util;
 public class Metadata {
     public static final String NAME = "MediaMod";
     public static final String MODID = "mediamod";
-    public static final String VERSION = "1.2.3";
-    public static final int VERSION_INT = 7;
+    public static final String VERSION = "1.2.4";
+    public static final int VERSION_INT = 8;
 }
