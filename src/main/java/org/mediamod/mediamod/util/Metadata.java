d9cf15c814b8206a1f6b8321e8811bb6ff3c8fc1
Conor Byrne <31479541+cbyrne@users.noreply.github.com>
2020-07-19 21:41:18 +0100

    Bump version to beta.2

--- src/main/java/org/mediamod/mediamod/util/Metadata.java
+++ src/main/java/org/mediamod/mediamod/util/Metadata.java
@@ -6,7 +6,7 @@ package org.mediamod.mediamod.util;
 public class Metadata {
     public static final String NAME = "MediaMod";
     public static final String MODID = "mediamod";
-    public static final String VERSION = "2.0.0-beta.1";
-    public static final int VERSION_INT = 2;
+    public static final String VERSION = "2.0.0-beta.2";
+    public static final int VERSION_INT = 10;
     public static final String MINECRAFT_VERSION = "1.8.9";
 }
