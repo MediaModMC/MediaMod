4008f208c797fd5502907e6ec23671487efabe9a
dreamhopping <71222289+dreamhopping@users.noreply.github.com>
2020-10-27 14:49:46 +0000

    Ignore SSL certificate when making requests with OkHttp - We must ignore SSL certificate when connecting to the MediaMod Backend as Minecraft's JRE doesn't like the SSL cert. This is insecure and will be fixed at a later date - I don't see any other solution at the moment

--- src/main/java/me/dreamhopping/mediamod/util/WebRequest.java
+++ src/main/java/me/dreamhopping/mediamod/util/WebRequest.java
@@ -1,8 +1,10 @@
 package me.dreamhopping.mediamod.util;
 
 import com.google.gson.Gson;
+import me.dreamhopping.mediamod.MediaMod;
 import okhttp3.*;
 
+import javax.net.ssl.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
@@ -10,8 +12,13 @@ import java.util.HashMap;
 public class WebRequest {
     public static final WebRequest instance = new WebRequest();
 
-    private final OkHttpClient client = new OkHttpClient();
+    private final OkHttpClient client;
     private final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    public WebRequest() {
+        OkHttpClient.Builder builder = configureToIgnoreCertificate(new OkHttpClient.Builder());
+        client = builder.build();
+    }
 
     public <T> T post(URL url, String body, Class<T> type) throws IOException {
         RequestBody requestBody = RequestBody.create(JSON, body);
@@ -96,4 +103,40 @@ public class WebRequest {
         Response response = client.newCall(request).execute();
         return response.code();
     }
+
+    private OkHttpClient.Builder configureToIgnoreCertificate(OkHttpClient.Builder builder) {
+        MediaMod.INSTANCE.logger.warn("Ignore Ssl Certificate");
+
+        try {
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+            builder.hostnameVerifier((hostname, session) -> true);
+        } catch (Exception e) {
+            MediaMod.INSTANCE.logger.warn("Exception while configuring IgnoreSslCertificate" + e, e);
+        }
+        return builder;
+    }
+
 }
