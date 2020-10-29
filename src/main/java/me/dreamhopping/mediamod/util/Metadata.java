d700759de73afa7bfd847272974d897c001177b9
dreamhopping <71222289+dreamhopping@users.noreply.github.com>
2020-10-29 13:49:53 +0000

    Bump version to 2.0.0-beta.8

--- src/main/java/me/dreamhopping/mediamod/util/Metadata.java
+++ src/main/java/me/dreamhopping/mediamod/util/Metadata.java
@@ -6,7 +6,7 @@ package me.dreamhopping.mediamod.util;
 public class Metadata {
     public static final String NAME = "MediaMod";
     public static final String MODID = "mediamod";
-    public static final String VERSION = "2.0.0-beta.7";
-    public static final int VERSION_INT = 15;
+    public static final String VERSION = "2.0.0-beta.8";
+    public static final int VERSION_INT = 16;
     public static final String MINECRAFT_VERSION = "1.8.9";
 }
