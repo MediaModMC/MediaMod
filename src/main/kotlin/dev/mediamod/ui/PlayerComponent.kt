346fa8b87fe8fa2e6b6badd483db8ecf21a97956
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-02-27 15:43:58 +0000

    ui: change background colour of player component and add artist text

--- src/main/kotlin/dev/mediamod/ui/PlayerComponent.kt
+++ src/main/kotlin/dev/mediamod/ui/PlayerComponent.kt
@@ -3,11 +3,9 @@ package dev.mediamod.ui
 import dev.mediamod.MediaMod
 import dev.mediamod.data.Track
 import gg.essential.elementa.UIComponent
-import gg.essential.elementa.components.UIImage
-import gg.essential.elementa.components.UIRoundedRectangle
-import gg.essential.elementa.components.UIText
-import gg.essential.elementa.components.Window
+import gg.essential.elementa.components.*
 import gg.essential.elementa.constraints.CenterConstraint
+import gg.essential.elementa.constraints.ChildBasedSizeConstraint
 import gg.essential.elementa.constraints.SiblingConstraint
 import gg.essential.elementa.dsl.*
 import gg.essential.elementa.state.BasicState
@@ -16,31 +14,36 @@ import java.awt.Color
 class PlayerComponent : UIComponent() {
     private val currentTrack = BasicState<Track?>(null)
 
-    private val background = UIRoundedRectangle(5f)
-        .constrain {
-            x = 5.pixels()
-            y = 5.pixels()
-            color = Color.darkGray.constraint
+    private val background = UIBlock(Color.darkGray.darker()).constrain {
+        x = 5.pixels()
+        y = 5.pixels()
 
-            width = 150.pixels()
-            height = 50.pixels()
-        } childOf this
+        width = 150.pixels()
+        height = 50.pixels()
+    } childOf this
 
-    private val imageContainer = UIRoundedRectangle(2.5f)
-        .constrain {
-            x = 5.pixels()
-            y = CenterConstraint()
-            color = Color(0, 0, 0, 0).constraint
+    private val imageContainer = UIContainer().constrain {
+        x = 5.pixels()
+        y = CenterConstraint()
+        color = Color(0, 0, 0, 0).constraint
 
-            width = 40.pixels()
-            height = 40.pixels()
-        } childOf background
+        width = 40.pixels()
+        height = 40.pixels()
+    } childOf background
 
-    private val trackNameText = UIText("Unknown Track")
-        .constrain {
-            x = SiblingConstraint(5f)
-            y = CenterConstraint()
-        } childOf background
+    private val textContainer = UIContainer().constrain {
+        x = SiblingConstraint(5f)
+        y = CenterConstraint()
+
+        height = ChildBasedSizeConstraint()
+    } childOf background
+
+    private val trackNameText = UIText("Unknown track") childOf textContainer
+
+    private val artistNameText = UIText("by Unknown artist").constrain {
+        color = Color.lightGray.constraint
+        y = SiblingConstraint(3f)
+    } childOf textContainer
 
     private var image = UIImage.ofResource("")
 
@@ -53,6 +56,7 @@ class PlayerComponent : UIComponent() {
             imageContainer.removeChild(image)
 
             trackNameText.setText(it?.name ?: "Unknown track")
+            artistNameText.setText("by ${it?.artist ?: "Unknown artist"}")
 
             image = it?.let { UIImage.ofURL(it.artwork) } ?: UIImage.ofResource("")
             image
