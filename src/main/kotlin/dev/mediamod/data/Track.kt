22375f34e09ff90a416c33b49167b5b88d5a52ec
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-02-27 15:48:07 +0000

    feat: add elapsed, duration, and paused to Track

--- src/main/kotlin/dev/mediamod/data/Track.kt
+++ src/main/kotlin/dev/mediamod/data/Track.kt
@@ -5,5 +5,8 @@ import java.net.URL
 data class Track(
     val name: String,
     val artist: String,
-    val artwork: URL
+    val artwork: URL,
+    val elapsed: Long,
+    val duration: Long,
+    val paused: Boolean
 )
--- src/main/kotlin/dev/mediamod/service/impl/TestService.kt
+++ src/main/kotlin/dev/mediamod/service/impl/TestService.kt
@@ -11,6 +11,9 @@ class TestService : Service {
     override fun pollTrack() = Track(
         "daisy",
         "still_bloom",
-        URL("https://lite-images-i.scdn.co/image/ab67616d0000b273bc10f89a8eb0fc4262848992")
+        URL("https://lite-images-i.scdn.co/image/ab67616d0000b273bc10f89a8eb0fc4262848992"),
+        0,
+        30000,
+        false
     )
 }
